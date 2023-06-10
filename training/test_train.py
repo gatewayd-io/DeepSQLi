3fdb937bff710b8e941acc27267ad5e48dce3f24
Mostafa Moradian <mostafa@gatewayd.io>
2023-06-10 18:36:20 +0200

    Add training model v1

---
+++ training/test_train.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import pytest
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+import numpy as np
+
+
+def test_sqli_model():
+    MAX_WORDS = 10000
+    MAX_LEN = 100
+
+    # Load dataset
+    data = pd.read_csv("dataset/sqli_dataset.csv")
+
+    # Load TF model from SavedModel
+    sqli_model = load_model("sqli_model")
+
+    # Create a sample SQL injection data
+    sample = [
+        "select * from users where id='1' or 1=1--",
+        "select * from users",
+        "select * from users where id=10000",
+        ("select * from test where id=1 UNION ALL "
+         "SELECT NULL FROM INFORMATION_SCHEMA.COLUMNS WHERE 1=0; --;"),
+    ]
+
+    # Tokenize the sample
+    tokenizer = Tokenizer(num_words=MAX_WORDS)
+    tokenizer.fit_on_texts(data["Query"])
+
+    # Vectorize the sample
+    sample_seq = tokenizer.texts_to_sequences(sample)
+    sample_vec = pad_sequences(sample_seq, maxlen=MAX_LEN)
+
+    # Predict sample
+    predictions = sqli_model.predict(sample_vec)
+    # Scale up to 100
+    assert predictions * 100 == pytest.approx(
+        np.array([[99.99], [0.005], [0.055], [99.99]]), 0.1
+    )
---
+++ training/train.py
@@ -0,0 +1,70 @@
+# Deep Learning Model Training with LSTM
+
+import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.models import save_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    accuracy_score,
+    recall_score,
+    precision_score,
+    f1_score,
+    confusion_matrix,
+)
+import numpy as np
+
+# Load dataset
+data = pd.read_csv("../model/sqli_dataset.csv")
+
+# Define parameters
+MAX_WORDS = 10000
+MAX_LEN = 100
+
+# Use Tokenizer to encode text
+tokenizer = Tokenizer(num_words=MAX_WORDS)
+tokenizer.fit_on_texts(data["Query"])
+sequences = tokenizer.texts_to_sequences(data["Query"])
+
+# Pad the text sequence
+X = pad_sequences(sequences, maxlen=MAX_LEN)
+
+# Split the training set and test set
+y = data["Label"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Create LSTM model
+model = Sequential()
+model.add(Embedding(MAX_WORDS, 128, input_length=MAX_LEN))
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation="sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# Train model
+model.fit(X_train, y_train, epochs=10, batch_size=32)
+
+# Predict test set
+y_pred = model.predict(X_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+# Calculate model performance indicators
+accuracy = accuracy_score(y_test, y_pred_classes)
+recall = recall_score(y_test, y_pred_classes, zero_division=1)
+precision = precision_score(y_test, y_pred_classes, zero_division=1)
+f1 = f1_score(y_test, y_pred_classes, zero_division=1)
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred_classes).ravel()
+
+# Output performance indicators
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+print("Recall: {:.2f}%".format(recall * 100))
+print("Precision: {:.2f}%".format(precision * 100))
+print("F1-score: {:.2f}%".format(f1 * 100))
+print("Specificity: {:.2f}%".format(tn / (tn + fp) * 100))
+print("ROC: {:.2f}%".format(tp / (tp + fn) * 100))
+
+# Save model as SavedModel format
+save_model(model, "../sqli_model")
