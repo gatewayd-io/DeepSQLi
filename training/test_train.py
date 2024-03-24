69bb4dfeb0785cc989001ce0ad858138e09eb568
Mostafa Moradian <mostafa@gatewayd.io>
2024-03-24 01:37:42 +0100

    Use TFSMLayer to load old SavedModel instances
    Update tests

--- training/test_train.py
+++ training/test_train.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pytest
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import TFSMLayer
 import numpy as np
 
 
@@ -31,7 +31,7 @@ def model(request):
     data = pd.read_csv(request.param["dataset"])
 
     # Load TF model from SavedModel
-    sqli_model = load_model(request.param["model_path"])
+    sqli_model = TFSMLayer(request.param["model_path"], call_endpoint="serving_default")
 
     # Tokenize the sample
     tokenizer = Tokenizer(num_words=MAX_WORDS, filters="")
@@ -47,11 +47,11 @@ def model(request):
 @pytest.mark.parametrize(
     "sample",
     [
-        ("select * from users where id=1 or 1=1;", [99.99, 0.11]),
-        ("select * from users where id='1' or 1=1--", [92.02, 0.11]),
-        ("select * from users", [0.077, 0.0044]),
-        ("select * from users where id=10000", [14.83, 0.10]),
-        ("select '1' union select 'a'; -- -'", [99.99, 99.99]),
+        ("select * from users where id=1 or 1=1;", [99.99, 99.83]),
+        ("select * from users where id='1' or 1=1--", [92.02, 99.83]),
+        ("select * from users", [0.077, 0.08]),
+        ("select * from users where id=10000", [14.83, 97.32]),
+        ("select '1' union select 'a'; -- -'", [99.99, 97.32]),
         (
             "select '' union select 'malicious php code' \g /var/www/test.php; -- -';",
             [99.99, 99.99],
@@ -68,10 +68,10 @@ def test_sqli_model(model, sample):
     sample_vec = pad_sequences(sample_seq, maxlen=MAX_LEN)
 
     # Predict sample
-    predictions = model["sqli_model"].predict(sample_vec)
+    predictions = model["sqli_model"](sample_vec)
 
     # Scale up to 100
-    print(predictions * 100)  # Debugging purposes (prints on error)
-    assert predictions * 100 == pytest.approx(
+    print(predictions["dense"].numpy() * 100)  # Debugging purposes (prints on error)
+    assert predictions["dense"].numpy() * 100 == pytest.approx(
         np.array([[sample[1][model["index"]]]]), 0.1
     )
