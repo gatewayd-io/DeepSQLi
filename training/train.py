3174613a406dbcf07f0dc81a2a5a0435ada33b08
Mostafa Moradian <mostafa@gatewayd.io>
2024-05-21 21:21:15 +0200

    Remove deprecated input_length
    Remove no verbosity

--- training/train.py
+++ training/train.py
@@ -83,7 +83,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Create LSTM model
 model = Sequential()
-model.add(Embedding(MAX_WORDS, 128, input_length=MAX_LEN))
+model.add(Embedding(MAX_WORDS, 128))
 model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation="sigmoid"))
 model.compile(
@@ -98,10 +98,10 @@ model.compile(
 )
 
 # Train model
-model.fit(X_train, y_train, epochs=11, batch_size=32, verbose=0)
+model.fit(X_train, y_train, epochs=11, batch_size=32)
 
 # Predict test set
-y_pred = model.predict(X_test, verbose=0)
+y_pred = model.predict(X_test)
 y_pred_classes = np.argmax(y_pred, axis=1)
 
 # Calculate model performance indicators
